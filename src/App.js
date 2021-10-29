7c94ac7ea866a3c6da2af864056b92f713dc45b3
Steve Silva <steveantoniosilva@gmail.com>
2021-10-29 08:19:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import tesla from "./tesla.js";
+import { Icon } from "leaflet";
+
+const teslaIcon = new Icon({
+  iconUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Tesla_T_symbol.svg/1200px-Tesla_T_symbol.svg.png",
+  iconSize: [33, 33],
+});
 
 function App() {
+  console.log(tesla);
+  const teslaUSA = tesla.filter(
+    (portugal) => portugal.address.country === "Portugal"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MapContainer
+      center={[39.736946, -4.142685]}
+      zoom={5.9}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      {teslaUSA.map((location) => (
+        <Marker
+          key={location.id}
+          icon={teslaIcon}
+          position={[location.gps.latitude, location.gps.longitude]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Popup position={[location.gps.latitude, location.gps.longitude]}>
+            <div>
+              <h2>{location.name}</h2>
+              <p>CHARGING STATIONS: {location.stallCount}</p>
+              <p>STATUS: {location.status}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
 
